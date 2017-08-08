e8f484d8a383ff39c05edf2c0280155c2629edd8
Christopher CHEN <fishdrowned@gmail.com>
2017-08-08 21:32:44 +0800

    docs: fulfill phwoolcon recipe

--- deploy.php
+++ deploy.php
@@ -1,8 +1,9 @@
 <?php
+
 namespace Deployer;
 
 require 'recipe/common.php';
-require __DIR__ . '/vendor/deployer/recipes/rsync.php';
+require 'vendor/deployer/recipes/recipe/rsync.php';
 
 // Configuration
 
@@ -14,34 +15,92 @@ set('writable_dirs', []);
 
 inventory('hosts.yml');
 
-host('prepare')->set('deploy_path', __DIR__ . '/working_dir');
+host('prepare')
+    ->stage('prepare')
+    ->set('deploy_path', __DIR__ . '/working_dir');
+
+set('rsync', [
+    'include' => [],
+    'exclude-file' => __DIR__ . '/working_dir/current/deployignore',
+    'exclude' => ['.git'],
+    'include-file' => false,
+    'filter' => [],
+    'filter-file' => false,
+    'filter-perdir' => false,
+    'flags' => 'auz',
+    'options' => [
+        'delete',
+        'rsync-path="sudo -u www-data rsync"',
+    ],
+    'timeout' => null,
+    'tty' => true,
+]);
+
+set('rsync_src', __DIR__ . '/working_dir/current');
+set('rsync_dest', '{{deploy_path}}');
 
 // Tasks
 
 task('deploy:rsync', function () {
-//    run('sudo systemctl restart php-fpm.service');
+    invoke('rsync');
+
+    $dst = get('rsync_dest');
+    while (is_callable($dst)) {
+        $dst = $dst();
+    }
+    cd($dst);
+    run('bin/dump-autoload');
+    run('bin/cli migrate:up');
+    run('bin/dump-autoload');
 });
 
 task('local:prepare_done', function () {
     writeln('<info>Local preparation Ok</info>');
 });
 
-task('git:ref', function () {
+task('git:last_ref', function () {
+    if (!is_dir(__DIR__ . '/working_dir/current')) {
+        return;
+    }
     cd('working_dir/current');
-    run('git branch -vv', ['tty' => true]);
+    set('last_ref', trim(run('git rev-parse HEAD')->getOutput()));
+});
+
+task('git:ref', function () {
+    if (!is_dir(__DIR__ . '/working_dir/current')) {
+        return;
+    }
+    cd('working_dir/current');
+    $branch = get('branch');
+    run("git reset --hard origin/{$branch}");
+    if ($lastRef = get('last_ref')) {
+        run($gitLog = "git log --pretty=format:'%C(green)%h%C(reset) [%C(yellow)%ci%C(reset)] - " .
+            "%an <%ae>%n%C(bold blue)%s%C(reset)%n%w(0,2,2)%b%n' {$lastRef}..HEAD", ['tty' => true]);
+        $changes = run($gitLog)->getOutput();
+        if (!trim($changes)) {
+            run('git branch -vv', ['tty' => true]);
+            writeln('  <comment>No Changes</comment>');
+        } else {
+            writeln('Changed files:');
+            run("git diff --name-status {$lastRef} HEAD", ['tty' => true]);
+        }
+    } else {
+        run('git branch -vv', ['tty' => true]);
+    }
 });
 
 desc('Prepare source code locally');
 task('local:prepare', [
     'deploy:prepare',
     'deploy:lock',
+    'git:last_ref',
     'deploy:release',
     'deploy:update_code',
-    'git:ref',
     'deploy:clear_paths',
     'deploy:symlink',
     'deploy:unlock',
     'cleanup',
+    'git:ref',
     'local:prepare_done',
 ])->onStage('prepare');
 
